d3255c6f6ae0cc12ddac6c02a658ade286ba9110
7978606023 <anubhavwhitebox@gmail.com>
2020-10-28 11:09:30 -0700

    updated

---
+++ precache-manifest.7dd265577c55284490ddd63ea20f77ea.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1d5dadd217037ef7b927a4d97c77dd1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b256b28f9dd2039d4b8d",
+    "url": "./static/css/2.2058ee32.chunk.css"
+  },
+  {
+    "revision": "3b12e5c2c4a2ba50be1e",
+    "url": "./static/css/main.db5ed23f.chunk.css"
+  },
+  {
+    "revision": "b256b28f9dd2039d4b8d",
+    "url": "./static/js/2.0b1f15a1.chunk.js"
+  },
+  {
+    "revision": "3b12e5c2c4a2ba50be1e",
+    "url": "./static/js/main.b315f0ff.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "d902400cd7f58620150ce21a91ea9b8c",
+    "url": "./static/media/Vmarine.d902400c.jpg"
+  },
+  {
+    "revision": "7656ed07a28aa28d65caf29ddc4dc1b9",
+    "url": "./static/media/aguaDeLuz.7656ed07.png"
+  },
+  {
+    "revision": "ac1aceade9946caaf83867ea7b70f2a0",
+    "url": "./static/media/myImage.ac1acead.png"
+  },
+  {
+    "revision": "3f235a834794b88f67a959880b5bdbb7",
+    "url": "./static/media/todolist.3f235a83.png"
+  }
+]);
